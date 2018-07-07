050d9c71e05d8de4803434d05fe12b2deab221ed
LIANGCA2 <CAROL T T LIANG (ITA-ISDC-ISD-OOCLL/ZHA) carol.t.t.liang@oocl.com>
2018-07-07 14:44:10 +0800

    optimize by lambda

--- main/main.js
+++ main/main.js
@@ -1,12 +1,125 @@
-// Write your cade below:
-function caclRemaindar() {};
+'use strict';
 
-function caclSum() {};
 
-function caclSumInConditon() {};
+const {loadAllItems, loadPromotions} = require('../spec/fixtures');
+
+function printReceipt(tags) {
+    let cartItemsArray = calculateByItemsNumber(tags);
+    let cartItemWithDetailArray = calCartItemsWithDetail(cartItemsArray, loadAllItems());
+    let sum = calculateCartItemSubTotalSum(cartItemWithDetailArray);
+    let saveMoneySum = saveItemMoneySum(cartItemWithDetailArray, sum);
+    let receipt = generateReceipt(cartItemWithDetailArray, sum, saveMoneySum);
+    console.log(receipt);
+}
+
+
+function buildCartItem(tags) {
+    return tags.map((tag) => {
+        if (tag.indexOf('-') !== -1) {
+            let [barcode, count] = tag.split('-');
+            return {
+                barcode,
+                count: parseFloat(count)
+            }
+        } else {
+            return {
+                barcode: tag,
+                count: 1
+            }
+        }
+    })
+}
+
+function mergeCartItem(cartItemList) {
+    let cartItemsMap = [];
+    cartItemList.forEach((cartItem) => {
+        const {barcode, count} = cartItem;
+        let existItem = findItemByBarcode(cartItemsMap, cartItem.barcode);
+        if (existItem != null) {
+            existItem.count = count + existItem.count;
+        } else {
+            cartItemsMap.push({
+                barcode,
+                count,
+            })
+        }
+    });
+    return cartItemsMap;
+}
+
+
+function calculateByItemsNumber(tags) {
+    let cartItemList = buildCartItem(tags);
+    return mergeCartItem(cartItemList);
+}
+
+function calculateSummary(item, count) {
+    if (loadPromotions()[0].barcodes.includes(item.barcode)) {
+        return item.price * (parseInt(count/3) * 2 + (count % 3));
+    }
+    else {
+        return item.price * parseFloat(count);
+    }
+}
+
+function findItemByBarcode(allItems, barcode) {
+    return allItems.find((item) => {
+        return item.barcode === barcode;
+    });
+}
+
+function calCartItemsWithDetail(cartItemsArray, allItems) {
+    return cartItemsArray.map((cartItem) => {
+        const {barcode, count} = cartItem;
+        const item = findItemByBarcode(allItems, cartItem.barcode);
+        const {name, price, unit} = item;
+        console.info(barcode);
+        return {
+            name,
+            count: count,
+            price,
+            summary: calculateSummary(item, count),
+            unit
+        }
+    })
+}
+
+function calculateCartItemSubTotalSum(cartItemWithDetailArray) {
+    let sum = 0;
+    cartItemWithDetailArray.forEach((item) => {
+        sum += item.summary;
+    });
+    return sum;
+}
+
+function saveItemMoneySum(cartItemWithDetailArray, sum) {
+    let originalPrice = 0;
+    cartItemWithDetailArray.forEach((item) => {
+        originalPrice += item.count * item.price;
+    });
+    return originalPrice - sum;
+}
+
+function generateReceipt(buyItemWithDetail, sum, saveMoneySum) {
+    let receipt = `***<没钱赚商店>收据***`;
+    for (let buyItem of buyItemWithDetail) {
+        receipt += `\n名称：${buyItem.name}，数量：${buyItem.count}${buyItem.unit}，单价：${buyItem.price.toFixed(2)}(元)，小计：${buyItem.summary.toFixed(2)}(元)`
+    }
+    receipt += `\n----------------------
+总计：${sum.toFixed(2)}(元)
+节省：${saveMoneySum.toFixed(2)}(元)
+**********************`;
+    return receipt;
+}
 
 module.exports = {
-    caclRemaindar,
-    caclSum,
-    caclSumInConditon
-}
+    printReceipt,
+    buildCartItem,
+    mergeCartItem,
+    calculateByItemsNumber,
+    calculateSummary,
+    calCartItemsWithDetail,
+    calculateCartItemSubTotalSum,
+    saveItemMoneySum,
+    generateReceipt
+};
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,26 +1,171 @@
 const main = require('../main/main');
+const { printReceipt,buildCartItem,mergeCartItem,calculateByItemsNumber,calculateSummary,calCartItemsWithDetail,calculateCartItemSubTotalSum,saveItemMoneySum,generateReceipt} = require('../main/main');
+const {loadAllItems,loadPromotions} = require('../spec/fixtures')
+describe('calCartItemsWithDetail', () => {
 
-describe('main()', () => {
+        it('should print array', () => {
+          const list = [{
+              barcode:'ITEM000000',
+              count:3
+          },
+              {
+                  barcode:'ITEM000001',
+                  count:2
+              }]
+           let result =  JSON.stringify(mergeCartItem(list));
+        const expectText = JSON.stringify(
+            [{
+                barcode:'ITEM000000',
+                count:3
+            },
+                {
+                    barcode:'ITEM000001',
+                    count:2
+                }]);
 
-    it('should calculate the remaindar', () => {
-        expect(main.caclRemaindar(9, 3)).toBe(0);
-        expect(main.caclRemaindar(11, 4)).toBe(3);
+        expect(result).toBe(expectText);
     });
 
-    it('should calculate the sum of numbers which are in array', () => {
-        let numArray1 = [3, 6, 1, 8, 6, 4, 2];
-        let numArray2 = [1, 2, 6, 2, 9, 0, 4];
-        
-        expect(main.caclSum(numArray1)).toBe(30);
-        expect(main.caclSum(numArray2)).toBe(24);
+
+
+
+
+
+    it('should print array', () => {
+
+        const cartItemsArray = [{
+            barcode:'ITEM000000',
+            count:3
+        },{
+          barcode:'ITEM000001',
+          count:2
+        }];
+        const allItems = loadAllItems();
+
+        let buyItemWithDetail = JSON.stringify( calCartItemsWithDetail(cartItemsArray,allItems))
+
+
+        const expectText = JSON.stringify(
+            [{name:'可口可乐',count:3,price:3.00,summary:6.00,unit:'瓶'},{name:'雪碧',count:2,price:3.00,summary:6.00,unit:'瓶'}]
+        );
+
+        expect(buyItemWithDetail).toBe(expectText);
     });
 
-    it('should calculate the sum of numbers which are in array and less than indicated num ', () => {
-        let numArray1 = [3, 6, 1, 8, 6, 4, 2];
-        let numArray2 = [1, 2, 6, 2, 9, 0, 4];
-        
-        expect(main.caclSumInConditon(numArray1 , 5)).toBe(10);
-        expect(main.caclSumInConditon(numArray2 , 6)).toBe(9);
+
+    it('should print map', () => {
+
+        const tags = [
+            'ITEM000000',
+            'ITEM000001',
+            'ITEM000000',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000003-2.5',
+            'ITEM000005',
+            'ITEM000005-2',
+        ];
+
+        let cartItemsMap = JSON.stringify(calculateByItemsNumber(tags));
+
+        const expectText = JSON.stringify([
+            {
+                barcode:'ITEM000000',
+            count:2
+            },
+            {
+                barcode:'ITEM000001',
+                count:3
+            },
+            {
+                barcode:'ITEM000003',
+                count:2.5
+            },{
+                barcode:'ITEM000005',
+                count:3
+            }
+        ]);
+
+        expect(cartItemsMap).toBe(expectText);
+    });
+
+    it('should print number', () => {
+
+        const cartItemList = [{name:'可口可乐',count:2,price:3.00,summary:6.00,unit:'瓶'},{name:'雪碧',count:1,price:3.00,summary:3.00,unit:'瓶'}];
+
+        let sum = JSON.stringify(calculateCartItemSubTotalSum(cartItemList))
+
+
+        const expectText = JSON.stringify(
+            9.00
+        );
+
+        expect(sum).toBe(expectText);
+    });
+
+
+        it('should print text', () => {
+
+            const itemList = [{name:'苹果',count:2.5,price:5.50,summary:13.75,unit:'斤'},{name:'方便面',count:3,price:4.50,summary:9.00,unit:'袋'}];
+            const sum =22.75;
+            const saveSum =4.50;
+            let text = generateReceipt(itemList,sum,saveSum);
+
+
+            const expectText = `***<没钱赚商店>收据***
+名称：苹果，数量：2.5斤，单价：5.50(元)，小计：13.75(元)
+名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+----------------------
+总计：22.75(元)
+节省：4.50(元)
+**********************`;
+
+            expect(text).toBe(expectText);
+        });
+
+    it('should print text', () => {
+
+        const tags = [
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000003-2.5',
+            'ITEM000005',
+            'ITEM000005-2',
+        ];
+
+        spyOn(console, 'log');
+
+        printReceipt(tags);
+
+        const expectText = `***<没钱赚商店>收据***
+名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
+名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
+名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+----------------------
+总计：58.50(元)
+节省：7.50(元)
+**********************`;
+
+        expect(console.log).toHaveBeenCalledWith(expectText);
+    });
+
+
+    it('should print number', () => {
+
+        const itemList = [{name:'苹果',count:2.5,price:5.50,summary:13.75,unit:'斤'},{name:'方便面',count:3,price:4.50,summary:9.00,unit:'袋'}];
+        const sum = 22.75;
+
+        let saveMoney = JSON.stringify(saveItemMoneySum(itemList,sum))
+
+
+        const expectText = JSON.stringify(
+            4.50
+        );
+
+        expect(saveMoney).toBe(expectText);
     });
 
 });
